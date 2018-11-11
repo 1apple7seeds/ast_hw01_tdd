76254b2eaf5bbe889f0c4ba6b87e0d99cd40c418
hkhbrus <hkran85@gmail.com>
2018-11-11 13:29:09 +0100

    Completed the testing code in class-Test1

--- src_test/testing/Test1.java
+++ src_test/testing/Test1.java
@@ -2,57 +2,78 @@ package testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class Test1 {
+import development.Question1;
 
-	@BeforeAll
-	static void setUp() {
+class Test1 {
+	
+	Question1 Q1;
+	int[] testList1;
+	int[] testList2;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		Q1 = new Question1();
+		testList1 = new int[] {1,2,3,4,5,6,7,8,9};
+		testList2 = new int[] {-20,-10,0,10,20};
 	}
 
-	@AfterAll
-	static void tearDown() {
+	@AfterEach
+	void tearDown() throws Exception {
+		Q1 = null;
+		testList1 = null;
+		testList2 = null;
 	}
 	
 	@Test
 	void user_input_test() {
-		fail("Not yet implemented");
+		assertEquals(50,Q1.user_input(50));	
+		Assertions.assertThrows(NumberFormatException.class, () -> {
+			Q1.user_input(101);
+		});
+		Assertions.assertThrows(NumberFormatException.class, () -> {
+			Q1.user_input(-1);
+		});
 	}
 	
-	@Test
-	void list_input_test() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	void summation_test() {
-		fail("Not yet implemented");
+		assertEquals(45,Q1.summation(testList1));	
+		assertEquals(0,Q1.summation(testList2));	
 	}
 	
 	@Test
 	void product_test() {
-		fail("Not yet implemented");
+		assertEquals(362880,Q1.product(testList1));	
+		assertEquals(0,Q1.product(testList2));
 	}
 	
 	@Test
 	void average_test() {
-		fail("Not yet implemented");
+		assertEquals(5,Q1.average(testList1));	
+		assertEquals(0,Q1.average(testList2));
 	}
 	
 	@Test
 	void variance_test() {
-		fail("Not yet implemented");
+		assertEquals((20.0/3),Q1.variance(testList1),0.001);	
+		assertEquals(200,Q1.variance(testList2));	
 	}
 	
 	@Test
 	void smallest_int_test() {
-		fail("Not yet implemented");
+		assertEquals(1,Q1.smallest_int(testList1));	
+		assertEquals(-20,Q1.smallest_int(testList2));	
 	}
 	
 	@Test
 	void largest_int_test() {
-		fail("Not yet implemented");
+		assertEquals(9,Q1.largest_int(testList1));	
+		assertEquals(20,Q1.largest_int(testList2));	
 	}	
 }
