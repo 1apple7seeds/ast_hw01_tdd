03cf2c83818a20ba5bcf256678ac1d6a1a09c41f
hkhbrus <hkran85@gmail.com>
2018-11-11 14:26:39 +0100

    Completed the testing code in class-Test3

--- src_test/testing/Test3.java
+++ src_test/testing/Test3.java
@@ -2,47 +2,66 @@ package testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import development.Question3;
 
 class Test3 {
 
-	@BeforeAll
-	static void setUp() {
+	Question3 Q3;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		Q3 = new Question3();
 	}
 
-	@AfterAll
-	static void tearDown() {
+	@AfterEach
+	void tearDown() throws Exception {
+		Q3 = null;
 	}
 	
 	@Test
 	void func_2n_test() {
-		fail("Not yet implemented");
+		assertEquals(0,Q3.func_2n()[0]);
+		assertEquals(100,Q3.func_2n()[50]);
+		assertEquals(200,Q3.func_2n()[100]);
 	}
 	
 	@Test
 	void func_sqrt_test() {
-		fail("Not yet implemented");
+		assertEquals(0,Q3.func_sqrt()[0]);
+		assertEquals(7,Q3.func_sqrt()[49]);
+		assertEquals(9,Q3.func_sqrt()[81]);
 	}
 
 	@Test
 	void func_10_pow_n_test() {
-		fail("Not yet implemented");
+		assertEquals("1",Q3.func_10_pow_n()[0]);
+		assertEquals("10",Q3.func_10_pow_n()[1]);
+		assertEquals("1000000",Q3.func_10_pow_n()[6]);
+		assertEquals("10...0 (99 zeros)",Q3.func_10_pow_n()[99]);
 	}
 	
 	@Test
 	void func_cubed_test() {
-		fail("Not yet implemented");
+		assertEquals(0,Q3.func_cubed()[0]);
+		assertEquals(125000,Q3.func_cubed()[50]);
+		assertEquals(1000000,Q3.func_cubed()[100]);
 	}
 	
 	@Test
 	void func_2_pow_1_over_n_test() {
-		fail("Not yet implemented");
+		assertEquals("Inf",Q3.func_2_pow_1_over_n()[0]);
+		assertEquals("2",Q3.func_2_pow_1_over_n()[1]);
+		assertEquals("1",Q3.func_2_pow_1_over_n()[100]);
 	}
 	
 	@Test
 	void func_e_pow_ntest() {
-		fail("Not yet implemented");
-	}
+		assertEquals("1",Q3.func_e_pow_n()[0]);
+		assertEquals("2",Q3.func_e_pow_n()[1]);
+		assertEquals("485165195",Q3.func_e_pow_n()[20]);
+		assertEquals("e^99",Q3.func_e_pow_n()[99]);	}
 }
